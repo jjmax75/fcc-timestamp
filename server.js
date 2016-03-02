8755cdd14c7321c05a022ee2e3a0ac30343dea78
John Behan <behanj@gmail.com>
2016-03-02 21:04:49 +0700

    get index with static dir setup

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 'use strict';
 
 var express = require('express');
+var routes = require('./app/routes/index.js');
 
 var app = express();
 var port = process.env.PORT || 8080;
 
-app.get('/', function(req, res) {
-	res.send('It works!');
-});
+app.use('/public', express.static(process.cwd() + '/public'));
+
+routes(app);
 
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
